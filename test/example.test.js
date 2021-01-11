f38a45c46e46b3d58da2f2dbe8181f0b208398de
Patrick Hrabos <phrabos@gmail.com>
2021-01-11 18:22:35 -0500

    adds tests for countsAsYes function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,42 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { countsAsYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should return true when provided a word that begins with letter y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = countsAsYes("yes to tests");
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('should return false when provided a word that begins with anything but y', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsYes("not it");
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('should return false when empty string is entered', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsYes("");
 
     //Expect
     // Make assertions about what is expected versus the actual result
